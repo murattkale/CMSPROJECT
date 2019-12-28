ee6fb6bcf1679aee5d6b8562d7ff844dd01c666e
MURAT KALE <murat.kale9339@gmail.com>
2019-12-29 00:42:01 +0300

    dfg

--- Entity/CMSDB/Brans.cs
+++ Entity/CMSDB/Brans.cs
@@ -5,14 +5,6 @@ namespace Entity.CMSDB
 {
     public partial class Brans : BaseModel
     {
-        public int Id { get; set; }
-        public int CreaUser { get; set; }
-        public DateTime CreaDate { get; set; }
-        public int? ModUser { get; set; }
-        public DateTime? ModDate { get; set; }
-        public int? OrderNo { get; set; }
-        public DateTime? IsDeleted { get; set; }
-        public int? IsStatus { get; set; }
         public string Ad { get; set; }
         public int KurumId { get; set; }
 
