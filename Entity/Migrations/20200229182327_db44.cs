4ab4b6bcba65b8e2746c918822015131f8a7978f
MURAT KALE <murat.kale9339@gmail.com>
2020-02-29 21:23:53 +0300

    fdgdf

---
+++ Entity/Migrations/20200229182327_db44.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Entity.Migrations
+{
+    public partial class db44 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
