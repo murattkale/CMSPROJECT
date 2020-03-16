b2c15c60c0fa27ece364ed0e59dafefd5e94aec0
MURAT KALE <murat.kale9339@gmail.com>
2020-03-17 01:11:55 +0300

    dwfds

--- ProductService/Service/Formlar/IFormlarService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Linq;
-using System.Linq.Expressions;
-using System.Collections.Generic;
-
-public interface IFormlarService : IGenericRepo<Formlar>
-    {
-        RModel<Formlar> InsertOrUpdate(Formlar model);
-    }
-
