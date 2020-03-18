9ed1e5b21b0113c1ced4c5029fdc10dd1e92abd6
MURAT KALE <murat.kale9339@gmail.com>
2020-03-18 19:07:02 +0300

    sdf

---
+++ EntityService/Migrations/20200318144303_db333dfffgffgfgfhf.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityService.Migrations
+{
+    public partial class db333dfffgffgfgfhf : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
